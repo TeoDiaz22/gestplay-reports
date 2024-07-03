811b67513ed580e109f6ad87a0739387d2d866f7
Mateo Díaz <mateo.diaz@epn.edu.ec>
2024-07-02 21:08:11 -0500

    feat: add example data

---
+++ src/static/profiles.js
@@ -0,0 +1,19 @@
+export const profiles = {
+    "values": [
+        {
+            "id": "bjh1ngm-cysv-h8bg-1lw00ohd6kv1",
+            "name": "John Doe",
+            "image": "https://randomuser.me/api/port"
+        },
+        {
+            "id": 2,
+            "name": "Jane Doe",
+            "image": "https://randomuser.me/api/port"
+        },
+        {
+            "id": 3,
+            "name": "John Smith",
+            "image": "https://randomuser.me/api/port"
+        },
+    ]
+}
