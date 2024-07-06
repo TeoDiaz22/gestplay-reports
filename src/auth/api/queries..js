d976deedd1509f4b5f5c7851deeea9299945d18b
Mateo Díaz <mateo.diaz@epn.edu.ec>
2024-07-06 09:07:27 -0500

    feat: add query to activate account

--- src/auth/api/queries..js
+++ src/auth/api/queries..js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const login = async (email, password) => {
-    return await axios.post(`http://localhost:8000/token`, {
+    return await axios.post(`${import.meta.env.VITE_URL_API}/token`, {
         username: email,
         password: password,
     }, {
@@ -9,4 +9,8 @@ export const login = async (email, password) => {
             "Content-Type": "application/x-www-form-urlencoded"
         }
     });
+};
+
+export const activateAccount = async (token) => {
+    return await axios.get(`${import.meta.env.VITE_URL_API}/confirm-email/${token}`);
 };
