b79dfe9a30a12033095e8d7fda6994415a4232be
Mateo Díaz <mateo.diaz@epn.edu.ec>
2024-07-06 09:34:29 -0500

    feat: add format to date

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,3 +3,7 @@ import moment from "moment/moment";
 export const formatDate = (dateString) => {
     return moment(dateString).format("DD/MM/YYYY HH:mm");
 };
+
+export const formatToOnlyDate = (dateString) => {
+    return moment(dateString).format("DD/MM/YYYY");
+}
