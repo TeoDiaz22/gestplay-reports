87456fada09857414d919ab74303d57291c3e122
Mateo Díaz <mateo.diaz@epn.edu.ec>
2024-07-10 19:51:10 -0500

    feat: add spacing in titles

--- src/modules/profile/components/StatsCharts.jsx
+++ src/modules/profile/components/StatsCharts.jsx
@@ -1,6 +1,7 @@
 import { AverangeErrorsByLevel } from "./charts-generals/AverangeErrorsByLevel";
 import { AverangePointsByLevel } from "./charts-generals/AverangePointsByLevel";
 import { AverangeTimeVsDate } from "./charts-generals/AverangeTimeVsDate";
+import { ErrorsByLevelPie } from "./charts-generals/ErrorsByLevelPie";
 import { PointsByTime } from "./charts-levels/PointsByTime";
 import { TimeByDate } from "./charts-levels/TimeByDate";
 import { TimeVsPoints } from "./charts-levels/TimeVsPoints";
@@ -12,16 +13,17 @@ export const StatsCharts = ({ stats, isAllLevels }) => {
         <>
             {isAllLevels ?
                 <>
-                    <h3>Estadisticas generales</h3>
+                    <h3 className="text-center m-3">Estadisticas generales</h3>
                     <div className="d-md-flex">
                         <AverangeErrorsByLevel stats={stats} />
                         <AverangePointsByLevel stats={stats} />
                     </div>
+                    <ErrorsByLevelPie stats={stats} />
                     <AverangeTimeVsDate stats={stats} />
                 </>
                 :
                 <>
-                    <h3>Estadisticas por nivel</h3>
+                    <h3 className="text-center m-3">Estadisticas por nivel</h3>
                     <TimeByDate stats={stats} />
                     <PointsByTime stats={stats} />
                     <TimeVsPoints stats={stats} />
